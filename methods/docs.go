a2f2245e582d6f2b04ce8394d072889bd7f8fd01
Athul Cyriac Ajay <athul8720@gmail.com>
2020-11-23 00:08:29 +0530

    :fire: Moved the whole to markdown and docsify

--- methods/docs.go
+++ methods/docs.go
@@ -15,10 +15,24 @@ func GenerateDocs(filename string) {
 	if err != nil {
 		log.Printf("Error Occured %v", err)
 	}
-	t := template.Must(template.ParseFiles(filepath.Join(cwd, "methods/templates/index.html")))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t.Execute(w, colls)
-	})
+	t := template.Must(template.ParseFiles(filepath.Join(cwd, "methods/templates/template.md")))
+
+	// Create the file
+	f, err := os.Create(filepath.Join(cwd, "methods/templates/README.md"))
+	if err != nil {
+		log.Printf("File Creation Error: %v", err)
+	}
+
+	// Execute the template to the file.
+	if err = t.Execute(f, colls); err != nil {
+		log.Println(err)
+	}
+
+	// Close the file when done.
+	f.Close()
+	fs := http.FileServer(http.Dir(filepath.Join(cwd, "methods/templates/")))
+	http.Handle("/", fs)
+
 	log.Printf("\033[1;36m%s\033[0m", "Server Listening at http://localhost:1341")
 	http.ListenAndServe(":1341", nil)
 }
